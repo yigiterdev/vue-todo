eeb181ec173bb6f8baec2f4a8a5c14037cbf3859
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2020-12-13 22:40:20 +0300

    add dist

---
+++ dist/js/about.e94c00c0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},r=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about"},[a("h1",[t._v("Hakkında")]),a("p",[t._v("Bu Todo App'i Traversy Media tutorial'ı ile yapılmıştır.")])])}],l=a("2877"),u={},i=Object(l["a"])(u,n,r,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=about.e94c00c0.js.map
