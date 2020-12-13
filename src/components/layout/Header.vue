15f14061fdcf5bb2ed295f027655d445b8c92c50
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2020-12-13 22:35:36 +0300

    add app

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Anasayfa</router-link> |
+      <router-link to="/about"> Hakkında</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Hakkında</h1>
+    <p>Bu Todo App'i Traversy Media tutorial'ı ile yapılmıştır.</p>
+  </div>
+</template>
